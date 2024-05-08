3809e009306706d69af27baa7ac0eac719286631
Pamela Santos <pamesantoss@hotmail.com>
2024-05-08 20:21:42 +0000

    feat: definição de cores e fontes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,4 +6,25 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme["brown-500"]};
+  }
+
+  body {
+    background-color: ${props => props.theme["gray-100"]};
+    color: ${props => props.theme["brown-500"]};
+  }
+
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  h1 {
+    font-family: "Poetsen One", sans-serif;
+    font-weight: 700;
+  }
 `
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,3 +1,23 @@
 export const defaultTheme = {
-  primary: 'purple'
+  white: '#FFF',
+
+  'gray-100': '#FAFAFA',
+  'gray-200': '#F3F2F2',
+  'gray-300': '#EDEDED',
+  'gray-400': '#E6E5E5',
+  'gray-500': '#D7D5D5',
+
+  'brown-100': '#8D8686',
+  'brown-500': '#574F4D',
+  'brown-700': '#403937',
+  'brown-900': '#272221',
+
+  'purple-300': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-700': '#4B2995',
+
+  'yellow-300': '#F1E9C9',
+  'yellow-700': '#DBAC2C',
+
+  'orange-300': '#C47F17'
 }
