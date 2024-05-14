67f40a46c87b8ce62fa2d39f29e2a822e6ed91c7
Pamela Santos <pamesantoss@hotmail.com>
2024-05-14 20:21:34 +0000

    feat: iniciando estilização do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,21 @@
+import { ActionButtonsContainer, HeaderContainer } from "./styles";
+import { Container } from "../../styles/default";
+
+import coffeeDeliveryLogo from "../../assets/coffee-delivery-logo.svg";
+
 export function Header() {
   return (
-    <h1>Header</h1>
+    <HeaderContainer>
+      <Container>
+        <div className="row">
+          <img src={coffeeDeliveryLogo} alt="Coffee Delivery" />
+
+          <ActionButtonsContainer>
+            <button>Location</button>
+            <button>Cart</button>
+          </ActionButtonsContainer>
+        </div>
+      </Container>
+    </HeaderContainer>
   );
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  padding: 2rem 0;
+
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+export const ActionButtonsContainer = styled.div``;
