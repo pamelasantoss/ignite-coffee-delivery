051529c9a300942bc4c5fe4b909fff0fee037d75
Pamela Santos <pamesantoss@hotmail.com>
2024-05-26 16:03:57 -0300

    feat: criação do formulário de endereço da página de checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,8 +1,11 @@
+import { MapPinLine } from "@phosphor-icons/react";
 import {
   CheckoutContainer,
   CheckoutInfoContainer,
   CheckoutLeftContainer,
-  CheckoutRightContainer
+  CheckoutRightContainer,
+  CheckoutTitleSection,
+  FormCheckoutSection
 } from "./styles";
 
 export function Checkout() {
@@ -12,7 +15,39 @@ export function Checkout() {
         <h2>Complete seu pedido</h2>
 
         <CheckoutInfoContainer>
-          Endereço
+          <CheckoutTitleSection>
+            <MapPinLine size={20} />
+            <p>
+              <span>Endereço de entrega</span>
+              Informe o endereço onde deseja receber seu pedido
+            </p>
+          </CheckoutTitleSection>
+
+          <FormCheckoutSection>
+            <form>
+              <div className="fieldset">
+                <input type="text" placeholder="CEP" className="input-cep" />
+              </div>
+
+              <div className="fieldset">
+                <input type="text" placeholder="Rua" />
+              </div>
+
+              <div className="fieldset">
+                <input type="number" placeholder="Número" className="input-numero" />
+                <div className="fieldset-complemento">
+                  <input type="text" placeholder="Complemento" />
+                  <span>Opcional</span>
+                </div>
+              </div>
+
+              <div className="fieldset">
+                <input type="text" placeholder="Bairro" className="input-bairro" />
+                <input type="text" placeholder="Cidade" />
+                <input type="text" placeholder="UF" className="input-uf" />
+              </div>
+            </form>
+          </FormCheckoutSection>
         </CheckoutInfoContainer>
 
         <CheckoutInfoContainer>
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -13,6 +13,30 @@ export const CheckoutContainer = styled.div`
     color: ${(props) => props.theme["brown-700"]};
     margin-bottom: 1.5rem;
   }
+
+  @media only screen and (max-width: ${(props) => props.theme["max-width"]}) {
+    padding: 1rem;
+    flex-direction: column;
+  }
+`;
+
+export const CheckoutLeftContainer = styled.div`
+  flex: 1;
+
+  @media only screen and (max-width: ${(props) => props.theme["max-width"]}) {
+    width: 100%;
+    margin-bottom: 2.5rem;
+  }
+`;
+
+export const CheckoutRightContainer = styled.div`
+  width: 40%;
+  margin-left: 2rem;
+
+  @media only screen and (max-width: ${(props) => props.theme["max-width"]}) {
+    width: 100%;
+    margin: 0;
+  }
 `;
 
 export const CheckoutInfoContainer = styled.div`
@@ -20,8 +44,92 @@ export const CheckoutInfoContainer = styled.div`
   border-radius: 6px;
   width: 100%;
   margin-bottom: 0.75rem;
+  padding: 2.5rem;
 `;
 
-export const CheckoutLeftContainer = styled.div``;
+export const CheckoutTitleSection = styled.div`
+  display: flex;
+  gap: 0.5rem;
 
-export const CheckoutRightContainer = styled.div``;
+  svg {
+    fill: ${(props) => props.theme["orange-300"]};
+  }
+
+  p {
+    font-size: 0.875rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+
+    span {
+      color: ${(props) => props.theme["brown-700"]};
+      font-size: 1rem;
+    }
+  }
+`;
+
+export const FormCheckoutSection = styled.div`
+  form {
+    margin-top: 2rem;
+
+    .fieldset {
+      padding: 0.5rem 0;
+      display: flex;
+      gap: 0.75rem;
+
+      input {
+        border-radius: 4px;
+        border: 1px solid ${(props) => props.theme["gray-400"]};
+        background-color: ${(props) => props.theme["gray-300"]};
+        width: 100%;
+        height: 42px;
+        display: flex;
+        align-items: center;
+        padding: 0 1rem;
+        font-size: 0.875rem;
+        color: ${(props) => props.theme["brown-500"]};
+
+        &:focus {
+          outline: 1px solid ${(props) => props.theme["orange-300"]};
+          box-shadow: none;
+        }
+
+        &.input-cep,
+        &.input-numero,
+        &.input-bairro {
+          width: 12.5rem;
+        }
+
+        &.input-uf {
+          width: 3.75rem;
+        }
+      }
+
+      .fieldset-complemento {
+        width: 100%;
+        position: relative;
+
+        span {
+          font-size: 0.75rem;
+          font-style: italic;
+          position: absolute;
+          right: 10px;
+          top: 35%;
+        }
+      }
+
+      @media only screen and (max-width: 600px) {
+        flex-direction: column;
+
+        input {
+          &.input-cep,
+          &.input-numero,
+          &.input-bairro,
+          &.input-uf {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+`;
