e0f860cb9f5638431ee62b639b29e2f27175b8a2
Pamela Santos <pamesantoss@hotmail.com>
2025-04-24 16:57:00 -0300

    fix: validação ao adicionar mais de um produto no carrinho

--- src/reducers/cart/reducer.ts
+++ src/reducers/cart/reducer.ts
@@ -4,27 +4,27 @@ import { addressFormData } from "../../components/AddressForm";
 import { products } from "../../json/products";
 
 export interface Product {
-  id: number,
-  image: string,
-  name: string,
-  description: string,
-  tags: string[],
-  price: number,
-  quantity: number,
+  id: number;
+  image: string;
+  name: string;
+  description: string;
+  tags: string[];
+  price: number;
+  quantity: number;
 }
 
 export interface CheckoutSummary {
-  items: number,
-  delivery: number,
-  total: number
+  items: number;
+  delivery: number;
+  total: number;
 }
 
 interface CartState {
-  cart: Product[],
-  productList: Product[],
-  address: addressFormData | null,
-  paymentMethod: string,
-  summary: CheckoutSummary
+  cart: Product[];
+  productList: Product[];
+  address: addressFormData | null;
+  paymentMethod: string;
+  summary: CheckoutSummary;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -37,7 +37,7 @@ export function cartReducer(state: CartState, action: any) {
   case ActionTypes.SUM_TO_CART:
     return produce(state, (draft) => {
       const currentProductList = draft.cart.findIndex(
-        product => product.id === action.payload.id
+        (product) => product.id === action.payload.id
       );
       draft.cart[currentProductList].quantity += 1;
       draft.productList[currentProductList].quantity += 1;
@@ -45,25 +45,28 @@ export function cartReducer(state: CartState, action: any) {
   case ActionTypes.UPDATE_QUANTITY:
     return produce(state, (draft) => {
       const currentProductList = draft.productList.findIndex(
-        product => product.id === action.payload.productId
+        (product) => product.id === action.payload.productId
       );
-      draft.productList[currentProductList].quantity = action.payload.quantity;
-      
+      draft.productList[currentProductList].quantity =
+          action.payload.quantity;
+
       if (draft.cart.length > 0) {
         const currentCart = draft.cart.findIndex(
-          product => product.id === action.payload.productId
+          (product) => product.id === action.payload.productId
         );
-        draft.cart[currentCart].quantity = action.payload.quantity;
+        if (currentCart !== -1) {
+          draft.cart[currentCart].quantity = action.payload.quantity;
+        }
       }
     });
   case ActionTypes.REMOVE_TO_CART:
     return produce(state, (draft) => {
       const indexToRemove = draft.cart.findIndex(
-        product => product.id === action.payload.productId
+        (product) => product.id === action.payload.productId
       );
 
       const indexToUpdate = draft.productList.findIndex(
-        product => product.id === action.payload.productId
+        (product) => product.id === action.payload.productId
       );
       if (indexToRemove !== -1) {
         draft.cart.splice(indexToRemove, 1);
@@ -71,9 +74,11 @@ export function cartReducer(state: CartState, action: any) {
       }
     });
   case ActionTypes.UPDATE_PRICE: {
-    const totalPriceItems = action.payload.reduce((
-      total: number, item: { price: number; quantity: number }
-    ) => total + (item.price * item.quantity), 0);
+    const totalPriceItems = action.payload.reduce(
+      (total: number, item: { price: number; quantity: number }) =>
+        total + item.price * item.quantity,
+      0
+    );
     const totalWithDelivery = totalPriceItems + state.summary.delivery;
     return produce(state, (draft) => {
       draft.summary.items = totalPriceItems;
