4634027a9d54f27624f3926948079151810129f8
Pamela Santos <pamesantoss@hotmail.com>
2025-06-09 15:18:58 -0300

    fix: remove tipagem desnecessária

--- src/components/PaymentButton/index.tsx
+++ src/components/PaymentButton/index.tsx
@@ -2,34 +2,28 @@ import { Bank, CreditCard, Money } from "@phosphor-icons/react";
 import { Payment } from "./styles";
 
 type PaymentButtonProps = {
-  name: "Cartão de crédito" | "Cartão de débito" | "Dinheiro",
-  icon: string,
-  isPaymentSelected: string,
-  onSelectionPayment: (type: string) => void
-}
+  name: string;
+  icon: string;
+  isPaymentSelected: string;
+  onSelectionPayment: (type: string) => void;
+};
 
 export function PaymentButton({
   name,
   icon,
   isPaymentSelected,
-  onSelectionPayment
+  onSelectionPayment,
 }: PaymentButtonProps) {
   return (
     <Payment
       onClick={() => onSelectionPayment(name)}
-      className={isPaymentSelected === name ? "active" :  ""}
+      className={isPaymentSelected === name ? "active" : ""}
     >
-      {icon === "creditCard" && (
-        <CreditCard size={16} />
-      )}
+      {icon === "creditCard" && <CreditCard size={16} />}
 
-      {icon === "bank" && (
-        <Bank size={16} />
-      )}
+      {icon === "bank" && <Bank size={16} />}
 
-      {icon === "money" && (
-        <Money size={16} />
-      )}
+      {icon === "money" && <Money size={16} />}
 
       {name}
     </Payment>
